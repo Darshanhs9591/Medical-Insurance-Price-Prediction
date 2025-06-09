6abef3eea52273645af1c9b3ff1def2af1ae272f
Darshanhs9591 <hsdarshan9591@gmail.com>
2025-06-09 15:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.ensemble import RandomForestRegressor
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
-
-import streamlit as st
-import pandas as pd
-import numpy as np
 import sys
 import traceback
 
